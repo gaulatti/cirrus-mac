e5ed71ee5229669c4f2bf229acbcff3d8120d759
Javier Godoy Núñez <javier@oxozon.io>
2025-02-22 13:47:52 -0500

    Initial Commit

---
+++ macTests/macTests.swift
@@ -0,0 +1,17 @@
+//
+//  macTests.swift
+//  macTests
+//
+//  Created by Javier Godoy Núñez on 2/22/25.
+//
+
+import Testing
+@testable import mac
+
+struct macTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
