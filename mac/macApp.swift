43a3f3a5467faeaad666670198c8f8ad01019e14
Javier Godoy Núñez <javier@oxozon.io>
2025-02-22 17:47:06 -0500

    landed: Icon and Window Title

--- mac/macApp.swift
+++ mac/macApp.swift
@@ -1,14 +1,19 @@
-import SwiftUI
 import SwiftData
+import SwiftUI
 
 @main
 struct macApp: App {
     @StateObject private var appState = AppState()
-       
-       var body: some Scene {
-           WindowGroup {
-               ContentView()
-                   .environmentObject(appState)
-           }
-       }
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(appState)
+                .onAppear {
+                    if let window = NSApplication.shared.windows.first {
+                        window.title = "Cirrus"
+                    }
+                }
+        }
+    }
 }
